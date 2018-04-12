54bcbc3b1f13b447a6ae7e08b77fb34cf29fabc9
Justyna Stepniak <stepniakj@gmail.com>
2018-04-12 20:27:10 +0200

    RGB code added to text results

--- basic-gui/index.py
+++ basic-gui/index.py
@@ -16,7 +16,7 @@ class Application(Frame):
                command = self.choose_color).grid(row=0, column=0, sticky=W)
 
         # text output
-        self.result = Text(self, width=30, height=2, wrap=WORD)
+        self.result = Text(self, width=30, height=3, wrap=WORD)
         self.result.grid(row=1, column=0, columnspan=2, sticky=W)
 
         # button that changes color
@@ -25,15 +25,19 @@ class Application(Frame):
 
     def choose_color(self):
         """ asks for a color and changes color of the button"""
-        color = askcolor()
-        self.color_name = color[1]
+        self.color = askcolor()
+        self.color_name = self.color[1]
         self.cb.configure(bg=self.color_name)
         self.update_txt()
 
     def update_txt(self):
-        color_result = self.color_name
-        message = ""
-        message += color_result
+        color_result_hex = self.color_name
+        rgb_tuple = self.color[0] #gets a tuple of RGB coordinates
+        color_result_rgb = ' '.join(format(x, "1.0f") for x in rgb_tuple) #transforms tuple into a string
+        message = "HEX code: "
+        message += color_result_hex
+        message += "\nRGB code: "
+        message += color_result_rgb
         self.result.delete(0.0, END)
         self.result.insert(0.0, message)
 
