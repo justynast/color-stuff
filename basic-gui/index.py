203765cf4a4f8f5f91491375cee75fdbcf51eec3
Justyna Stepniak <stepniakj@gmail.com>
2018-04-14 13:09:18 +0200

    change_color function deleted

--- basic-gui/index.py
+++ basic-gui/index.py
@@ -23,15 +23,11 @@ class Application(Frame):
         self.cb = Button(self, height=1, width=20)
         self.cb.grid(row=0, column=1, sticky=W)
 
-    def change_color(self):
-        """ changes color of the button """
-        self.cb.configure(bg=self.color_name)
-
     def choose_color(self):
-        """ asks for a color"""
+        """ asks for a color and changes color of the button"""
         self.color = askcolor()
         self.color_name = self.color[1]
-        self.change_color()
+        self.cb.configure(bg=self.color_name)
         self.update_txt()
 
     def update_txt(self):
