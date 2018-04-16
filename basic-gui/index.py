b2841549f7bcc1c26ac783961d52ab4ab493f99e
Justyna Stepniak <stepniakj@gmail.com>
2018-04-16 21:43:48 +0200

    added color name to output

--- basic-gui/index.py
+++ basic-gui/index.py
@@ -31,22 +31,29 @@ class Application(Frame):
     def choose_color(self):
         """ asks for a color and changes color of the button"""
         self.color = askcolor()
-        self.color_name = self.color[1]
-        self.cb.configure(bg=self.color_name)
+        self.color_hex = self.color[1]
+        self.cb.configure(bg=self.color_hex)
+        self.get_name()
         self.update_txt()
 
+    def get_name(self):
+        if self.color_hex in color_dict:
+            self.color_name = color_dict[self.color_hex]
+        else:
+            self.color_name = 'unknown'
+
     def update_txt(self):
-        color_result_hex = self.color_name
+        color_result_hex = self.color_hex
         rgb_tuple = self.color[0] #gets a tuple of RGB coordinates
         color_result_rgb = ' '.join(format(x, "1.0f") for x in rgb_tuple) #transforms tuple into a string
         message = "HEX code: "
         message += color_result_hex
         message += "\nRGB code: "
         message += color_result_rgb
+        message += "\nColor name: "
+        message += self.color_name
         self.result.delete(0.0, END)
         self.result.insert(0.0, message)
-
-
 
 root = Tk()
 root.title("Color Picker")
